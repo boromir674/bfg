cf4be327b79b2c6892373bc99607182c7f7998d8
Konstantinos <k.lampridis@hotmail.com>
2017-08-27 14:24:01 +0200

    implement Inventory which can register and manage items, figures, resources

--- bfg-engine/src/utils/Inventory.java
+++ bfg-engine/src/utils/Inventory.java
@@ -1,8 +1,6 @@
 package utils;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import bfgElements.mechanics.Resource;
@@ -10,28 +8,96 @@ import bfgElements.physical.BfgItem;
 import bfgElements.physical.Figure;
 
 /**
- * This class serves as the personal manager of all the players belongings (cards, resources, tokens, etc).
+ * This class serves as the personal manager of all the players belongings (cards, resources, tokens, etc). It registers Items and resources.
  * @author kostadinos
  */
 public class Inventory {
 	
-	private List<BfgItem> items;
+	private Map<String, BfgItem> items;
 	private Map<String, Integer> resources;
-	private List<Figure> figures;
+	private Map<String, Figure> figures;
 	
-	public Inventory() {
-		this.items = new ArrayList<BfgItem>();
+	private Inventory() {
+		this.items = new HashMap<String, BfgItem>();
 		this.resources = new HashMap<String, Integer>();
-		this.figures = new ArrayList<Figure>();
+		this.figures = new HashMap<String, Figure>();
 	}
 	
+	/**
+	 * Factory method. Initializes a fresh empty Inventory instance.
+	 * @return the instance
+	 */
+	public Inventory initialize() {
+		return new Inventory();
+	}
+//	public void initialize() {
+//		this.items = new ArrayList<BfgItem>();
+//		this.resources = new HashMap<String, Integer>();
+//		this.figures = new ArrayList<Figure>();
+//	}
+	/**
+	 * Call this method to register an item (card, figure, token, ..) object in the inventory. 
+	 * @param item the newly inserted into the inventory
+	 */
 	public void add(BfgItem item) {
-		this.items.add(item);
+		if (item.getClass().equals(Figure.class)) {
+			this.figures.put(item.getLabel(), (Figure) item);
+		}
+		else {
+			this.items.put(item.getLabel(), item);
+		}
 	}
-	public void add(Figure figure) {
-		this.figures.add(figure);
+	
+	/**
+	 * Call this method to request a reference to a registered item (card, token, figure, ..) object in the inventory.
+	 * @param label the unique identifier key
+	 * @return the reference to the item if found. Returns null if the object is not found.
+	 */
+	public BfgItem get(String label) {
+		BfgItem found = this.items.get(label);
+		if (found != null)
+			return found;
+		else {
+			return this.figures.get(label);
+		}
 	}
+	
+	/**
+	 * Call this method to remove a registered item from the inventory. If not found nothing happens.
+	 * @param label the unique identifier key
+	 */
+	public void remove(String label) {
+		BfgItem found = this.items.get(label);
+		if (found != null)
+			this.items.remove(label);
+		else {
+			this.figures.remove(label);
+		}
+	}
+	
+	/**
+	 * Call this method to remove the specified bulk of a registered resource collected into the inventory. If not found nothing happens.
+	 * @param resource an object encapsulating the bulk of the resource to be removed
+	 */
+	public void remove(Resource resource) {
+		String l = resource.getLabel();
+		if (resources.containsKey(l)) {
+			resources.put(l, resources.get(l) - resource.getValue());
+		}
+	}
+	
+	/**
+	 * Call this method to add the specified bulk of resource into the inventory.
+	 * @param resource an object encapsulating the bulk of the resource to be added
+	 */
 	public void add(Resource resource) {
-		if (resources.containsKey(resource.getValue()))
+		String l = resource.getLabel();
+		if (resources.containsKey(l)) {
+			resources.put(l, resources.get(l) + resource.getValue());
+		}
+		else {
+			resources.put(l, resource.getValue());
+		}
 	}
+	
 }
