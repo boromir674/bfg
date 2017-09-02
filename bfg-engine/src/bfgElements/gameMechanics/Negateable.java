616b9d7bc4f6220ec026968cff2c1d98e0a906e5
Konstantinos <k.lampridis@hotmail.com>
2017-09-02 20:53:15 +0200

    define Negateable interface

---
+++ bfg-engine/src/bfgElements/gameMechanics/Negateable.java
@@ -0,0 +1,14 @@
+package bfgElements.gameMechanics;
+
+/**
+ * A class implementing this interface allows for being negated.
+ * @author kostadinos
+ */
+public interface Negateable {
+	
+	/**
+	 * This method reports whether the object has been negated or not. 
+	 * @return the boolean flag. True for negated object
+	 */
+	public boolean isNegated();
+}
